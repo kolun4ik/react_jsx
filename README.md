cf48ece9269253fba8d1025eaddaefa1acd8b907
Th0rn <11_ka@rambler.ru>
2019-12-08 09:49:09 +0300

    Start codding

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 Practice, practice, practice!!!!
+
+Create simple React application with semantic-ui.
+
+
+[semantic-ui](https://semantic-ui.com/)
+
--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
+
 const App = () => {
-    return <div>Hi everi oneQ</div>;
+
+    const buttonText = { text: 'Click Me'};
+    const labelText = 'Enter name:';
+
+    return (
+        <div>
+            <label className="label" htmlFor="name">
+                {labelText}
+            </label>
+            <input id="name" type="text"/>
+    <button style={{backgroundColor: 'blue', color: 'white'}}>{buttonText.text}</button>
+        </div>
+        );
 };
 
 ReactDOM.render(
